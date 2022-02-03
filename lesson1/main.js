4ea820d7de5cc3f8a89d039146f64ce88aee8ebc
wow4uk <wow4uk83@gmail.com>
2022-02-03 20:39:51 +0200

    add lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,60 @@
+// let number = 100500;
+// console.log(number+200);
+// console.log(typeof number);
+// let sommsg = 'привіт мене .';
+// console.log(typeof sommsg)
+// let PI = 3.14;
+// PI = 5252
+// console.log(PI)
+
+// // {
+// //     var a = 'hello'
+// //     let b = 'world'
+// // }
+// //     console.log(a)
+// //     console.log(b)
+// console.log(true)
+// console.log(false);
+// console.log(5>6)
+// console.log(typeof true)
+// let bool2 = 5 === '5'
+// console.log(typeof bool2)
+// let num = 100500
+// let numbers = [100, 200, 300, num]
+// console.log(numbers)
+// console.log(numbers[0])
+// console.log(numbers[3])
+// console.log(numbers.length)
+// let arr = ['a', 'b', 'c', [11, 22, 33]]
+// console.log(arr[3][2])
+// let titleProduct1 = 'milk'
+// let price1 = 23
+// let quantity1 = 2
+// let product1 = {
+//     title: 'milk',
+//     price: 23,
+//     quantity: 2
+// }
+// console.log(product1)
+// console.log(product1.title)
+// console.log(product1.quantity)
+// let product2 = {
+//     title: 'beer',
+//     price: 36,
+//     quantity: 3,
+//     manufacture: {
+//         title: 'saninBev',
+//         city: 'chernigiv',
+//         zip: 43025,
+//         conservants: [{id : 'a10',sts:1}, {id : 'b20',sts:2}, {id : 'c30',sts:3}]
+//     }
+// }
+// console.log(product2.title)
+// console.log(product2['title'])
+// console.log(product2.manufacture.title)
+// console.log(product2.manufacture.conservants[1])
+// console.log(product2.manufacture.conservants[1].sts)
+// console.log(10 % 3)
+console.log(10+'5')
+console.log(10-'2')
+console.log(NaN)
