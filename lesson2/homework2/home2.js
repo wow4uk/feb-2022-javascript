075c74243ac5d2eb3421fb1084f51525fb18cc8c
wow4uk <wow4uk83@gmail.com>
2022-02-21 13:35:18 +0200

    add home2

---
+++ lesson2/homework2/home2.js
@@ -0,0 +1,40 @@
+// if (time >= 0 && time <= 15) {
+//     console.log('Перша чверть')
+// } else if (time >= 15 && time <= 29) {
+//     console.log('Друга чверть')
+// } else if (time > 30 && time <= 44) {
+//     console.log('Друга чверть')
+// } else if (time>=45 && time <=59){
+//     console.log('Четверта чверть')
+// } else{
+//     console.log('некоректно введені дані')}
+//
+// let day = +prompt('введіть число місяця')
+// if (day >= 1 && day <= 10) {
+//     console.log('Перша декада')
+// }
+// if (day >= 11 && day <= 20) {
+//     console.log('Друга декада')
+// }
+// if (day>=21 && day<=31){
+//     console.log('Третя декада')
+// } else {
+//     console.log("Некоректно введені дані")}
+// let test = 'asf';
+// if (test === true) {
+//     console.log('Вірно')
+// } else {
+//     console.log("Невірно")}
+// let test = !!'' ? 'Невірно' : 'Вірно'
+// console.log(test);
+
+// let a = +prompt('Введіть число')
+// if (a !== 0) {
+//     console.log('Вірно')
+// } else {
+//     console.log('Невірно')};
+// (a!=0) ? 'Вірно' : 'Невірно';
+
+
+
+
